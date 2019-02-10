f8a3ee9640adcb04af45707f620d355e96c1d4ec
Gilson da Gama <gilsondagama@hotmail.com>
2019-02-10 15:39:10 -0300

    Troca de Shelf, de State e Book OK. Faltando atualizar State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import ListBooks from './ListBooks'
+import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    books: []        
+class BooksApp extends Component {
+  state = { books: [] }
+   
     /*showSearchPage: false */
-  }
-
+ 
   componentDidMount() {
     BooksAPI.getAll()    
       .then((books) => {
@@ -18,14 +17,28 @@ class BooksApp extends React.Component {
       })
   }
 
-  changeShelf(book, shelf) {
-    console.log('BOOK ID', book);
-    console.log('SELECTED SHELF', shelf);
-  }
+  changeShelf = (book, selectShelf) => {
+    let { books } = this.state; 
+    
+    console.log('STATE - ANTES', books)    
+    console.log('BOOK ID', book.book.id); 
+    console.log('BOOK', book);
 
+    books=books.filter(b => b.id !== book.book.id).concat({
+      ...book.book,      
+      shelf: selectShelf
+    }); 
+    
+    console.log('STATE', books)         
+    console.log('SELECTED SHELF', selectShelf);
+
+    this.setState({ books });
+  }  
+  
   render() {
+    const { books = [] } = this.state;
+
     return (
-   
       <div className="app">
         {this.state.showSearchPage ? (
           <div className="search-books">
@@ -48,18 +61,18 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <div>
                 <div>
-                  <ListBooks books={this.state.books} shelfs='currentlyReading' bookshelftitle='Currently Reading' 
-                    onChangeShelf={this.changeShelf}
+                  <ListBooks books={books} shelfs='currentlyReading' bookshelftitle='Currently Reading' 
+                    changeShelf={this.changeShelf}
                   />        
                 </div>
                 <div>
-                  <ListBooks books={this.state.books} shelfs='wantToRead' bookshelftitle='Want To Read'
-                    onChangeShelf={this.changeShelf}
+                  <ListBooks books={books} shelfs='wantToRead' bookshelftitle='Want To Read'
+                    changeShelf={this.changeShelf}                    
                   />        
                 </div>
                 <div>
-                  <ListBooks books={this.state.books} shelfs='read' bookshelftitle='Read'        
-                    onChangeShelf={this.changeShelf}
+                  <ListBooks books={books} shelfs='read' bookshelftitle='Read'        
+                    changeShelf={this.changeShelf}                    
                   />
                 </div>
               </div>
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,73 +1,54 @@
-import React, { Component } from 'react'
-//import PropTypes from 'prop-types'
+import React from 'react'
 //import { Link } from 'react-router-dom'
-
 import './App.css'
 
-class ListBooks extends Component {
-  /*
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onChangeShelf: PropTypes.func.isRequired,
+const ListBooks =({books, shelfs, bookshelftitle, changeShelf}) => {
+  const showingBooks = books.filter((b) => b.shelf === shelfs)
+
+  function SelectShelf(getElement) {   
+    let strShelf = getElement.options[getElement.selectedIndex].text;  
+    console.log('STRShelf', strShelf)
+    return strShelf
   }
-  */
-  render () {
-    const { books, shelfs, bookshelftitle } = this.props   
-    
-    //console.log('BOOKS', books)
-    //console.log('STATE', this.props);
 
-    const showingBooks = books.filter((b) => b.shelf === shelfs)
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{bookshelftitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+        
+          {showingBooks.map((book) => (
+            <li key = {book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, 
+                    backgroundImage:`url(${book.imageLinks.smallThumbnail})` }}>
+                  </div>   
 
-    function SelectShelf(getElement) {
-      let strShelf = getElement.options[getElement.selectedIndex].text;
-      return strShelf
-    }
+                  <div className="book-shelf-changer">
+                    <select id="book-shelf-changer" 
+                      onChange={() => {changeShelf({book}, 
+                              SelectShelf(document.getElementById("book-shelf-changer")))}}>
 
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{bookshelftitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-          
-            {showingBooks.map((book) => (
-              <li key = {book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, 
-                      backgroundImage:`url(${book.imageLinks.smallThumbnail})` }}>
-                    </div>   
-
-                    <div className="book-shelf-changer">
-                      <select id="book-shelf-changer" 
-                        onClick={() => this.props.onChangeShelf(book.id, 
-                                SelectShelf(document.getElementById("book-shelf-changer")))}>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>                                            
-                        <option value="none">None</option>
-                      </select>
-                    </div>
+                      <option value="move" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>                                            
+                      <option value="none">None</option>
+                    </select>
                   </div>
-                  <div className="book-title">{ book.title }</div>
-                  <div className="book-authors">{ book.authors }</div>
                 </div>
-              </li>
-            ))}
-          </ol>
-
-        </div>
+                <div className="book-title">{ book.title }</div>
+                <div className="book-authors">{ book.authors }</div>
+              </div>
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
-/*
-ListBooks.PropTypes = {
-  books: PropTypes.array.isRequired,
-  onChangeShelf: PropTypes.func.isRequired
-}
-*/
+
 export default ListBooks
 
 
