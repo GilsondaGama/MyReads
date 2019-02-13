e80681b737c3b346c5748f542f3ab4d704c9cf16
Gilson da Gama <gilsondagama@hotmail.com>
2019-02-13 00:29:49 -0300

    Rating com estrelad implementado (Somente para visualização, API sem suporte para persistência de Rating)

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ class BooksApp extends Component {
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <button className="close-search" 
+                onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">              
                 <input type="text" placeholder="Search by title or author"/>
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -6,9 +6,18 @@ const ListBooks =({books, shelfs, bookshelftitle, changeShelf}) => {
   const showingBooks = books.filter((b) => b.shelf === shelfs)
 
   function SelectShelf(getElement) {   
-    let strShelf = getElement.options[getElement.selectedIndex].value;  
-    console.log('STRShelf', strShelf)
+    let strShelf = getElement.options[getElement.selectedIndex].value; 
     return strShelf
+  }
+
+  function RatingBook(bookTitle) {
+    let letra = bookTitle.substring(0,1).toUpperCase()
+    if    (['1','6','A','F','K','P','U','Z'].indexOf(letra) !== -1) {  return '1'
+    } else if (['2','7','B','G','L','Q','V'].indexOf(letra) !== -1) {  return '2'
+    } else if (['3','8','C','H','M','R','W'].indexOf(letra) !== -1) {  return '3'
+    } else if (['4','9','D','I','N','S','X'].indexOf(letra) !== -1) {  return '4'            
+    } else if (['5','0','E','J','O','T','Y'].indexOf(letra) !== -1) {  return '5'
+    } else { return '0' }
   }
 
   return (
@@ -27,15 +36,20 @@ const ListBooks =({books, shelfs, bookshelftitle, changeShelf}) => {
 
                   <div className="book-shelf-changer">
                     <select id={book.id} 
-                      onChange={() => {changeShelf({book}, 
-                              SelectShelf(document.getElementById(book.id)))}}>
+                        onChange={() => {changeShelf({book}, 
+                                SelectShelf(document.getElementById(book.id)))}}>
 
                       <option value="move" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>                                            
-                      <option value="none">None</option>
+                      <option value="currentlyReading" >Currently Reading</option>
+                      <option value="wantToRead" >Want to Read</option>
+                      <option value="read" >Read</option>                                            
+                      <option value="none" >None</option>
                     </select>
+                  </div>
+                  <div className="book-rating-star">
+                    <div className="book-rating-number">
+                      {RatingBook(book.title)}
+                    </div>
                   </div>
                 </div>
                 <div className="book-title">{ book.title }</div>
