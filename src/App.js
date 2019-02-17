5e1e07c5c65d5bd8eae1c20445b2bba4ad9034bd
Gilson da Gama <32863365+GilsondaGama@users.noreply.github.com>
2019-02-17 02:26:08 -0300

    Add files via upload
    Projeto Terminado, com (Rating, Loading) como algo a mais.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom';
+import LoadingOverlay from 'react-loading-overlay';
+
 import * as BooksAPI from './BooksAPI'
+
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends Component {
   state = { books: [],
-            showSearchPage: false }
+            isLoading: false }
  
   componentDidMount() {
     BooksAPI.getAll()    
@@ -18,63 +22,78 @@ class BooksApp extends Component {
   }
 
   changeShelf = (book, selectShelf) => {
+    this.startLoading();
     let { books } = this.state; 
     books=books.filter(b => b.id !== book.book.id).concat({
       ...book.book,      
       shelf: selectShelf
     }); 
 
-    BooksAPI.update(book, selectShelf) 
-      .then(() => {  })
-      .catch(() => { alert('Something went wrong with your request.'); })
+    BooksAPI.update(book.book, selectShelf) 
+      .catch(() => { alert('Something went wrong with your request.'); })   
+      .then(this.endLoading);
  
     this.setState({ books });
+  } 
+  
+  startLoading = () => {
+    this.setState({ isLoading: true });
+  };
 
-    console.log('BOOK', book)
-    console.log('SELECSHELF', selectShelf)
-  }  
+  endLoading = () => {
+    this.setState({ isLoading: false });
+  };
 
   render() {
     const { books = [] } = this.state
-    const { showSearchPage } = this.state.showSearchPage
     
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks books={books} changeShelf={this.changeShelf} 
-                      showSearchPage={showSearchPage}
-          />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
+
+        <LoadingOverlay
+          active={this.state.isLoading}
+          spinner
+          text="Loading..."
+        >
+          <Route exact path="/search" render={() => (
+            <SearchBooks books={books} changeShelf={this.changeShelf}/>
+          )}/>
+
+          <Route exact path="/" render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>My Reads</h1>
+              </div>
+              <div className="list-books-content">
                 <div>
-                  <ListBooks books={books} shelfs='currentlyReading' bookshelftitle='Currently Reading' 
-                    changeShelf={this.changeShelf}
-                  />        
-                </div>
-                <div>
-                  <ListBooks books={books} shelfs='wantToRead' bookshelftitle='Want To Read'
-                    changeShelf={this.changeShelf}                    
-                  />        
-                </div>
-                <div>
-                  <ListBooks books={books} shelfs='read' bookshelftitle='Read'        
-                    changeShelf={this.changeShelf}                    
-                  />
+                  <div>
+                    <ListBooks books={books} shelfs='currentlyReading' bookshelftitle='Currently Reading' 
+                      changeShelf={this.changeShelf}
+                    />        
+                  </div>
+                  <div>
+                    <ListBooks books={books} shelfs='wantToRead' bookshelftitle='Want To Read'
+                      changeShelf={this.changeShelf}                    
+                    />        
+                  </div>
+                  <div>
+                    <ListBooks books={books} shelfs='read' bookshelftitle='Read'        
+                      changeShelf={this.changeShelf}                    
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
-      </div>      
 
+              <div className="open-search">
+                <div className="open-search button">
+                  <Link to="/search">Search</Link>
+                </div>
+              </div>
+            
+            </div>
+          )}/>
+        </LoadingOverlay>      
+      </div>      
     )
   }
 }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
-//import { Link } from 'react-router-dom'
-import './App.css'
+import { Link } from 'react-router-dom'
+
 import RatingBook from './RatingBook'
 import SelectShelf from './SelectShelf'
+import './App.css'
 
 class SearchBooks extends Component { 
 
   state = { query: '',
-            updateQuery: '',
-            showSearchPage: false }      
+            updateQuery: ''}      
 
   updateQuery = (query) => {
     this.setState(() => ({
-      query:query.trim()
+      query:query
     }))
   }
   
@@ -29,13 +29,12 @@ class SearchBooks extends Component {
     : books.filter((b) => (
       b.title.toLowerCase().includes(query.toLowerCase())
       ))    
-      
 
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <button className="close-search" 
-            onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <Link className="close-search" to="/">Close</Link>
+
           <div className="search-books-input-wrapper">              
             <input type="text" placeholder="Search by title or author"
               value = { query }
@@ -72,7 +71,6 @@ class SearchBooks extends Component {
                           SelectShelf(document.getElementById(book.id)))}}
                           defaultValue={book.shelf}
                       >
-
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading" >Currently Reading</option>
                         <option value="wantToRead" >Want to Read</option>
